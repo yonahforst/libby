e240281e49f81ae023d5dfd6f89403e385f6b79e
Yonah Forst <yonaforst@hotmail.com>
2020-07-22 10:44:20 +0200

    Revert "support logging strings directly"
    This reverts commit 831ea65fb1fd4568c3f53844c97f4de2868aa0b8.

--- log/index.js
+++ log/index.js
@@ -41,11 +41,6 @@ function log(level, message, params) {
     context = requestContext.get()
   }
 
-  // accept string messages
-  if (typeof message == 'string') {
-    message = { message }
-  }
-
   // default to DEBUG
   const levelSet = context.logLevel || process.env.logLevel || 'INFO'
 
