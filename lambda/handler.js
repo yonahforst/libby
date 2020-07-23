bb25d1b239533c138d01a4ee44b45a432a8983b9
Yonah Forst <yonaforst@hotmail.com>
2020-07-23 09:25:30 +0200

    allow lambda handler with no context (testing)

--- lambda/handler.js
+++ lambda/handler.js
@@ -1,7 +1,7 @@
 const requestContext = require('./_requestContext')
 const log = require('./_log')
 
-module.exports = func => (event, context) => {
+module.exports = func => (event, context={}) => {
   if (requestContext && context.clientContext) {
     requestContext.replace(context.clientContext)
     log.debug('lambda.handler: set requestContext from inbound ClientContext')
